ffa46aff0f1e64a8c6a42fb51c1ce6b630494809
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-11-02 00:51:06 +0100

    feat/contact-user-model

---
+++ model/contact.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const ContactSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  firstName: { type: String, trim: true, required: true, maxlength: 200 },
+  lastName: { type: String, trim: true, required: true, maxlength: 2000 },
+  phoneNumber: { type: Number, required: true},
+  date: { type: Date, default: Date.now },
+})
+
+module.exports = mongoose.model('Contact', ContactSchema)
