bf18d10259cb7ad1e12139250e52388ca72c1a9e
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-11-02 12:00:04 +0100

    feat/add-context-central

--- controller/Contact/deleteContact.js
+++ controller/Contact/deleteContact.js
@@ -4,18 +4,18 @@ module.exports = async (req, res) => {
   try {
     //find the note by delete
     let contact = await Contact.findById(req.params.id)
-    if (!Contact) {
+    if (!contact) {
       res.status(404).send('Not Found')
     }
 
     // allow deletion
 
-    if (Contact.user.toString() !== req.user.id) {
+    if (contact.user.toString() !== req.user.id) {
       return res.status(401).send('Not allowed')
     }
 
-    contact = await Contact.findByIdAndDelete(req.params.id)
-    res.json({ success: 'deleted Contact' })
+    await Contact.findByIdAndDelete(req.params.id)
+    res.json({ success: 'Deleted Contact' })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: error.message })
